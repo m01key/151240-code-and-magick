3572c7b5c2668d7e55692a5048772b0090e4ada5
mars <maspam@yandex.ru>
2018-04-20 23:08:09 +0300

    модуляция

--- js/setup.js
+++ js/setup.js
@@ -1,198 +1,197 @@
 'use strict';
 
-// КОНСТАНТЫ
-var FIRST_NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
+(function () {
 
-var LAST_NAMES = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
+  var FIRST_NAMES = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
 
-var COAT_COLORS = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
+  var LAST_NAMES = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
 
-var EYES_COLORS = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
 
-var FIREBALL_COLORS = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
+  var EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
 
-var MAGES_AMOUNT = 4;
+  var FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
 
-
-// ПЕРЕМЕННЫЕ (пути)
-var mageItemTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var mageListElement = document.querySelector('.setup-similar-list');
-var setupElement = document.querySelector('.setup');
-var setupCoatElement = setupElement.querySelector('.wizard-coat');
-var setupCoatValElement = setupElement.querySelector('input[name=coat-color]');
-var setupEyesElement = setupElement.querySelector('.wizard-eyes');
-var setupEyesValElement = setupElement.querySelector('input[name=eyes-color]');
-var setupFireBallElement = setupElement.querySelector('.setup-fireball-wrap');
-var setupFireBallValElement = setupElement.querySelector('input[name=fireball-color]');
-var artifactsShopElement = setupElement.querySelector('.setup-artifacts-shop');
-var artifactsElement = setupElement.querySelector('.setup-artifacts');
-var artifact;
+  var MAGES_AMOUNT = 4;
 
 
-// ФУНКЦИИ
-function getRandElem(arr) {
-  var randomIndex = Math.random() * arr.length;
-  randomIndex = Math.floor(randomIndex);
+  var mageItemTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var mageListElement = document.querySelector('.setup-similar-list');
+  var setupElement = document.querySelector('.setup');
+  var setupCoatElement = setupElement.querySelector('.wizard-coat');
+  var setupCoatValElement = setupElement.querySelector('input[name=coat-color]');
+  var setupEyesElement = setupElement.querySelector('.wizard-eyes');
+  var setupEyesValElement = setupElement.querySelector('input[name=eyes-color]');
+  var setupFireBallElement = setupElement.querySelector('.setup-fireball-wrap');
+  var setupFireBallValElement = setupElement.querySelector('input[name=fireball-color]');
+  var artifactsShopElement = setupElement.querySelector('.setup-artifacts-shop');
+  var artifactsElement = setupElement.querySelector('.setup-artifacts');
+  var artifact;
 
-  return arr[randomIndex];
-}
 
-/**
- * Создает магов
- * @return {array} - Массив из MAGES_AMOUNT магов
- */
-function createMages() {
-  var mages = [];
+  function getRandElem(arr) {
+    var randomIndex = Math.random() * arr.length;
+    randomIndex = Math.floor(randomIndex);
 
-  for (var i = 0; i < MAGES_AMOUNT; i++) {
-    mages[i] = {
-      name: getRandElem(FIRST_NAMES) + ' ' + getRandElem(LAST_NAMES),
-      coatColor: getRandElem(COAT_COLORS),
-      eyesColor: getRandElem(EYES_COLORS)
-    };
+    return arr[randomIndex];
   }
 
-  return mages;
-}
+  /**
+   * Создает магов
+   * @return {array} - Массив из MAGES_AMOUNT магов
+   */
+  function createMages() {
+    var mages = [];
 
-/**
- * Создает DOM-элемент мага
- * @param  {object} data - Данные для шаблона
- * @return {DOMElement}  - Маг
- */
-function createMageDOMElement(data) {
-  var mage = mageItemTemplate.cloneNode(true);
-  mage.querySelector('.setup-similar-label').textContent = data.name;
-  mage.querySelector('.wizard-coat').style.fill = data.coatColor;
-  mage.querySelector('.wizard-eyes').style.fill = data.eyesColor;
+    for (var i = 0; i < MAGES_AMOUNT; i++) {
+      mages[i] = {
+        name: getRandElem(FIRST_NAMES) + ' ' + getRandElem(LAST_NAMES),
+        coatColor: getRandElem(COAT_COLORS),
+        eyesColor: getRandElem(EYES_COLORS)
+      };
+    }
 
-  return mage;
-}
-
-/**
- * Создает DOM-элементы магов и вставляет в разметку
- * @param  {array} data - Данные для шаблона
- */
-function insertDOMElements(data) {
-  var fragment = document.createDocumentFragment();
-
-  for (var i = 0; i < data.length; i++) {
-    var mage = createMageDOMElement(data[i]);
-    fragment.appendChild(mage);
+    return mages;
   }
 
-  mageListElement.appendChild(fragment);
-}
+  /**
+   * Создает DOM-элемент мага
+   * @param  {object} data - Данные для шаблона
+   * @return {DOMElement}  - Маг
+   */
+  function createMageDOMElement(data) {
+    var mage = mageItemTemplate.cloneNode(true);
+    mage.querySelector('.setup-similar-label').textContent = data.name;
+    mage.querySelector('.wizard-coat').style.fill = data.coatColor;
+    mage.querySelector('.wizard-eyes').style.fill = data.eyesColor;
 
-
-// СОБЫТИЯ
-setupEyesElement.addEventListener('click', function () {
-  var color = getRandElem(EYES_COLORS);
-  setupEyesElement.style.fill = color;
-  setupEyesValElement.value = color;
-});
-
-setupFireBallElement.addEventListener('click', function () {
-  var color = getRandElem(FIREBALL_COLORS);
-  setupFireBallElement.style.backgroundColor = color;
-  setupFireBallValElement.value = color;
-});
-
-setupCoatElement.addEventListener('click', function () {
-  var color = getRandElem(COAT_COLORS);
-  setupCoatElement.style.fill = color;
-  setupCoatValElement.value = color;
-});
-
-artifactsShopElement.addEventListener('dragstart', function (e) {
-  artifact = e.target.cloneNode();
-  artifactsElement.style.outline = '2px dashed red';
-});
-
-artifactsShopElement.addEventListener('dragend', function () {
-  artifactsElement.style.outline = '';
-});
-
-artifactsElement.addEventListener('dragstart', function (e) {
-  artifact = e.target;
-  artifactsElement.style.outline = '2px dashed red';
-});
-
-artifactsElement.addEventListener('dragend', function () {
-  artifactsElement.style.outline = '';
-});
-
-artifactsElement.addEventListener('dragenter', function (e) {
-  var target = e.target;
-
-  if (!target.closest('.setup-artifacts-cell').firstChild) {
-    target.style.backgroundColor = 'yellow';
+    return mage;
   }
-});
 
-artifactsElement.addEventListener('dragleave', function (e) {
-  var target = e.target;
+  /**
+   * Создает DOM-элементы магов и вставляет в разметку
+   * @param  {array} data - Данные для шаблона
+   */
+  function insertDOMElements(data) {
+    var fragment = document.createDocumentFragment();
 
-  target.style.backgroundColor = '';
-});
+    for (var i = 0; i < data.length; i++) {
+      var mage = createMageDOMElement(data[i]);
+      fragment.appendChild(mage);
+    }
 
-artifactsElement.addEventListener('dragover', function (e) {
-  e.preventDefault();
-});
-
-artifactsElement.addEventListener('drop', function (e) {
-  e.preventDefault();
-  var target = e.target;
-
-  artifactsElement.style.outline = '';
-  target.style.backgroundColor = '';
-
-  if (!target.closest('.setup-artifacts-cell').firstChild) {
-    e.target.appendChild(artifact);
+    mageListElement.appendChild(fragment);
   }
-});
 
 
-// ВЫПОЛНЕНИЕ
-document.querySelector('.setup-similar').classList.remove('hidden');
+  setupEyesElement.addEventListener('click', function () {
+    var color = getRandElem(EYES_COLORS);
+    setupEyesElement.style.fill = color;
+    setupEyesValElement.value = color;
+  });
 
-var mages = createMages();
-insertDOMElements(mages);
+  setupFireBallElement.addEventListener('click', function () {
+    var color = getRandElem(FIREBALL_COLORS);
+    setupFireBallElement.style.backgroundColor = color;
+    setupFireBallValElement.value = color;
+  });
+
+  setupCoatElement.addEventListener('click', function () {
+    var color = getRandElem(COAT_COLORS);
+    setupCoatElement.style.fill = color;
+    setupCoatValElement.value = color;
+  });
+
+  artifactsShopElement.addEventListener('dragstart', function (e) {
+    artifact = e.target.cloneNode();
+    artifactsElement.style.outline = '2px dashed red';
+  });
+
+  artifactsShopElement.addEventListener('dragend', function () {
+    artifactsElement.style.outline = '';
+  });
+
+  artifactsElement.addEventListener('dragstart', function (e) {
+    artifact = e.target;
+    artifactsElement.style.outline = '2px dashed red';
+  });
+
+  artifactsElement.addEventListener('dragend', function () {
+    artifactsElement.style.outline = '';
+  });
+
+  artifactsElement.addEventListener('dragenter', function (e) {
+    var target = e.target;
+
+    if (!target.closest('.setup-artifacts-cell').firstChild) {
+      target.style.backgroundColor = 'yellow';
+    }
+  });
+
+  artifactsElement.addEventListener('dragleave', function (e) {
+    var target = e.target;
+
+    target.style.backgroundColor = '';
+  });
+
+  artifactsElement.addEventListener('dragover', function (e) {
+    e.preventDefault();
+  });
+
+  artifactsElement.addEventListener('drop', function (e) {
+    e.preventDefault();
+    var target = e.target;
+
+    artifactsElement.style.outline = '';
+    target.style.backgroundColor = '';
+
+    if (!target.closest('.setup-artifacts-cell').firstChild) {
+      e.target.appendChild(artifact);
+    }
+  });
+
+
+  document.querySelector('.setup-similar').classList.remove('hidden');
+
+  var mages = createMages();
+  insertDOMElements(mages);
+
+})();
 
